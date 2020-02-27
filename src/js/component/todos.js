7de658f765b6dda22a9c52ea4b8114624523ac6b
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-27 12:15:19 -0500

    working

--- src/js/component/todos.js
+++ src/js/component/todos.js
@@ -1,49 +1,58 @@
 import React, { useState } from "react";
-
 export function Todos() {
-	const [tasks, setTasks] = useState([]);
-
-	const deleteLabel = ind => {
-		const newTasks = [...tasks];
-		newTasks.splice(ind, 1);
-		setTasks(newTasks);
-		console.log(newTasks);
-	};
-
-	return (
-		<div className="container d-flex justify-content-center p-5">
-			<div className="row">
-				<div className="col">
-					<h1>TODO list</h1>
-					<input
-						placeholder="Add a task"
-						className="Input"
-						onKeyUp={e =>
-							e.keyCode === 13 &&
-							setTasks(
-								tasks.concat({
-									label: e.target.value
-								})
-							)
-						}
-					/>
-					<div className="list-group">
-						{tasks === null
-							? "Loading..."
-							: tasks.map((t, index, myarr) => (
-									<a
-										href="#"
-										className="list-group-item list-group-item-action d-flex justify-content-left"
-										key={index}
-										onClick={() => {
-											deleteLabel(index);
-										}}>
-										{t.label}
-									</a>
-							  ))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    const [tasks, setTasks] = useState([]);
+    const [value, setValue] = useState("");
+    const deleteLabel = ind => {
+        const newTasks = [...tasks];
+        newTasks.splice(ind, 1);
+        setTasks(newTasks);
+        console.log(newTasks);
+    };
+    const onValueChange = ({ target: { value } }) => {
+        setValue(value);
+    };
+    const update = e => {
+        e.preventDefault();
+        setValue("");
+    };
+    return (
+        <div className="container d-flex justify-content-center p-5">
+            <div className="row">
+                <div className="col">
+                    <h1>TODO list</h1>
+                    <form onSubmit={update}>
+                        <input
+                            value={value}
+                            onChange={onValueChange}
+                            placeholder="Add a task"
+                            className="Input"
+                            onKeyDown={e =>
+                                e.keyCode === 13 &&
+                                setTasks(
+                                    tasks.concat({
+                                        label: e.target.value
+                                    })
+                                )
+                            }
+                        />
+                    </form>
+                    <div className="list-group">
+                        {tasks === null
+                            ? "Loading..."
+                            : tasks.map((t, index, myarr) => (
+                                    <a
+                                        href="#"
+                                        className="list-group-item list-group-item-action d-flex justify-content-left"
+                                        key={index}
+                                        onClick={() => {
+                                            deleteLabel(index);
+                                        }}>
+                                        {t.label}
+                                    </a>
+                              ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
